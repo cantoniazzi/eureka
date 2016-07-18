6e6da1bbadb6a82a8538b79e44868f46929d09dd
agreve <agreve@CIWRKST06E0B01>
2016-07-18 16:42:31 -0300

    Add database file

---
+++ Eureka/Eureka.Infra/Context/MyDatabase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eureka.Infra.Context
+{
+    public class MyDatabase : DbContext
+    {
+        public MyDatabase()
+            : base("ConnectionStringName")
+        {
+
+        }
+    }
+}
